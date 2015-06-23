b29413783307ff7d4d77fccc291dfee85cf2fdb0
andrewkhouzam <andrew.khouzam@gmail.com>
2015-06-23 16:05:00 +0200

    Modified the code of the transition

--- qabila/js/index.js
+++ qabila/js/index.js
@@ -1,3 +1,64 @@
+function redirectFromCollapsedToHomepage()
+{
+	$(".menu-outer").removeClass('collapse');
+	$(".menu").removeClass("collapse");
+	$(".menu img").attr("src","img/menuLines.png");
+	
+	$(".gifs-container.collapse").removeClass('transition');
+	// $(".container .gif-image:after").css("border","none");
+
+	$.ajax({
+	  url: "homepage.html",
+	  // context: document.body
+	}).success(function(r) {
+	  // console.log(r);
+
+	  c = $(r);
+	});
+
+	setTimeout(function(){
+	$(".gifs-container:not(.collapse)").show();
+	$(".gifs-container.collapse").hide();
+	$("body").removeClass();
+	$("body").addClass('homepage');
+
+		// console.log(c);
+		$("body").html(c);
+
+	},1000);
+}
+
+function redirectFromHomepageTocollapsed()
+{
+	$(".menu-outer").addClass('collapse');
+	$(".menu").addClass("collapse");
+	$(".menu img").attr("src","img/collapseMenuLines.png");
+	
+	
+	// $(".container .gif-image:after").css("border","none");
+
+	$.ajax({
+	  url: "collapsed.html",
+	  // context: document.body
+	}).success(function(r) {
+	  // console.log(r);
+
+	  collapsed = $(r);
+	});
+
+	setTimeout(function(){
+	$("body").html(collapsed);
+	collapsedPageInit();
+	$(".gifs-container.collapse").addClass('transition');
+	$("body").removeClass();
+	$("body").addClass('collapse');
+
+		// console.log(c);
+		
+
+	},200);
+}
+
 
 $(document).ready(function(){
 		$(document).on("click", ".menu-outer", function(){
@@ -5,12 +66,20 @@ $(document).ready(function(){
 //Redirecting from the collapsed Menu to the last viewed page
 if($("body").hasClass('collapse'))
 {
+	// Redirecting from collapsed view to Homepage
+	if(bodyClasses=="landing")
+	{
+		redirectFromCollapsedToHomepage();		
+	}
 
 }
 // redirecting from landingPage (index.html) to the collapsed page (collapsed.html)
 else if($("body").hasClass('landing'))
 {	
 	$('body').load('collapsed.html', function() {
+		//The code here is executed after loading
+		bodyClasses = $("body").attr("class");
+		// console.log(bodyClasses);
 		$('body').removeClass();
 		$('body').addClass('collapse');
 		collapsedPageInit();
@@ -20,12 +89,13 @@ else if($("body").hasClass('landing'))
 // redirecting from homepage (homepage.html) to the collapsed page (collapsed.html)
 else if($("body").hasClass('homepage')){
 
-	$('body').load('collapsed.html', function() {
-		$('body').removeClass();
-		$('body').addClass('collapse');
-		collapsedPageInit();
-		$(".gifs-container.collapse").addClass('transition');
-	});
+redirectFromHomepageTocollapsed();
+	// $('body').load('collapsed.html', function() {
+	// 	$('body').removeClass();
+	// 	$('body').addClass('collapse');
+	// 	collapsedPageInit();
+	// 	$(".gifs-container.collapse").addClass('transition');
+	// });
 }
 	})
 })
@@ -68,6 +138,7 @@ jQuery(document).ready(function($) {
 	});
 	$("#circle").click(function(event) {
 		/* Act on the event */
+	bodyClasses = $("body").attr("class");
 	$("body").removeClass();	
 	$("body").addClass('homepage');
 	$("body").html(homepage);
@@ -82,34 +153,8 @@ jQuery(document).ready(function($) {
 	$(".gif-image#gif-1").click(function(event) {
 
 		/* Act on the event */
-	$(".menu-outer").removeClass('collapse');
-	$(".menu").removeClass("collapse");
-	$(".menu img").attr("src","img/menuLines.png");
-	
-	$(".gifs-container.collapse").removeClass('transition');
-	$(".container .gif-image:after").css("border","none");
-
-	$.ajax({
-	  url: "homepage.html",
-	  // context: document.body
-	}).success(function(r) {
-	  // console.log(r);
-
-	  c = $(r);
-	});
-
-	setTimeout(function(){
-	$(".gifs-container:not(.collapse)").show();
-	$(".gifs-container.collapse").hide();
-	$("body").removeClass("collapse");
-	$("body").addClass('homepage');
-
-		// console.log(c);
-		$("body").html(c);
-
-	},1000);
-
-
+		redirectFromCollapsedToHomepage();
 	});
 });
+
 
